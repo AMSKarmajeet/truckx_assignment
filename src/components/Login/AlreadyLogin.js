0dbe4fc515237b0bea36a90cba4b75e472796d75
AMSKarmajeet <kcbhakta.99@gmail.com>
2021-02-26 14:17:55 +0530

    Authentication page updates

--- src/components/Login/AlreadyLogin.js
+++ src/components/Login/AlreadyLogin.js
@@ -7,6 +7,8 @@ export default function AlreadyLogin() {
   return(
     <div className="login-wrapper">
       <h1>You are Authenticated</h1>
+      <h2>Logout Page under construction...</h2>
+      <h2><bold id="gre">For now!</bold> clear your browser cookies for this site, or check it on incognito mode.</h2>
     </div>
   )
 
